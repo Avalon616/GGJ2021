6ace814fb1db75aca3ca95a81b9c95bb73985296
Kagooick <nubbernaut@gmail.com>
2021-01-30 14:47:48 -0500

    progress towards goals

---
+++ Assets/System.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    [CreateAssetMenu(fileName ="NewSystem", menuName ="ScripObj/System")]
+    public class System : ScriptableObject
+    {
+        public string DisplayName;
+        public List<Action> actions;
+        public GameObject AppearenceObject;
+    }
+}
